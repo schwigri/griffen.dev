47c760dfc731eed90713920083ade7131a64994e
Griffen Schwiesow <schwigri@pm.me>
2021-04-03 23:20:28 -0700

    feat: expand gatsby hosting support

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
 		title: "Griffen Schwiesow",
 	},
 	plugins: [
+		"gatsby-plugin-gatsby-cloud",
 		"gatsby-plugin-image",
 		"gatsby-plugin-sharp",
 		"gatsby-transformer-sharp",
